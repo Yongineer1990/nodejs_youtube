25864439384ac3457560bb029d8ac092a6bde4dc
yongineer1990@gmail.com <60580491+Yongineer1990@users.noreply.github.com>
2020-09-06 04:48:07 +0900

    ADD : middleware PART.1

--- index.js
+++ index.js
@@ -4,10 +4,28 @@ const app = express();
 const PORT = 8000;
 
 const handleListening = () => console.log(`Listening on : http://localhost:${PORT}`);
-const handleHome = (req, res) => res.send('hello, world!');
-const hendleProfile = (req, res) => res.send('profile');
+const handleDetail = (req, res) => {
+  console.log('/detail');
+  res.send('detail');
+};
+const handleHome = (req, res) => {
+  console.log('root');
+  res.send('hello, world!');
+};
+const hendleProfile = (req, res) => {
+  console.log('/profile');
+  res.send('profile');
+};
+const middleware = (req, res, next) => {
+  console.log('this is middleware');
+  next();
+};
 
 app.listen(PORT, handleListening);
 
 app.get('/', handleHome);
+
+app.use(middleware);
+
+app.get('/detail', handleDetail);
 app.get('/profile', hendleProfile);
